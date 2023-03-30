666c44fa08e699f29ea482f36353f4af5d54f4dc
ZettaiNi <vojta.ondracka@gmail.com>
2023-03-30 20:27:04 +0200

    casutikarychle

--- header.php
+++ header.php
@@ -1,3 +1,73 @@
 <header>
-    <a href="user.php"><p style="font-size: 1.5rem;color:white;opacity:0.7;">Příhlásit se</p></a>
+
+    
+    
+
+    <div class="form-popup" id="prihlaseni">
+        <form action="" class="form-container">
+            <h2 style="font-size:2rem;margin:0 0 1rem 0">Přihlášení</h2>
+            <input type="text" placeholder="Uživatelské jméno" name="username" class="username" required>
+            <input type="password" placeholder="Heslo" name="pwd" class="password" required>
+
+            <button type="submit" class="login">Login</button>
+            <button type="button" class="close" onclick="closePrihlaseni()">Close</button>
+            
+        </form>
+        <button id="btnReg" onclick="openRegistrace()">Registrace</button>
+    </div>
+
+    <div class="form-popup" id="registrace">
+        <form action="" class="form-container">
+            <h2 style="font-size:2rem;margin:0 0 1rem 0">Registrace</h2>
+            <input type="text" placeholder="Uživatelské jméno" name="username" class="username" required>
+            <input type="text" placeholder="Email" name="email" class="email" required>
+            <input type="password" placeholder="Heslo" name="pwd" class="password" required>
+            <input type="password" placeholder="Opakuj heslo" name="pwdRepear" class="password" required>
+
+            <button type="submit" class="registrace">Registrovat</button>
+            <button type="button" class="close" onclick="closeRegistrace()">Close</button>
+
+        </form>
+        
+    </div>
+
+    <button id="btnPri" onclick="openPrihlaseni()">Přihlásit se</button>
+
+    <!-- <a href="profil.php">
+        <p style="font-size: 1.5rem;color:white;opacity:0.7;">Přihlásit se</p>
+    </a> -->
 </header>
+<script>
+    document.getElementById("prihlaseni").style.display = "none";
+    document.getElementById("registrace").style.display = "none";
+    document.getElementById("btnPri").style.display = "block";
+    document.getElementById("btnReg").style.display = "none";
+
+    function openPrihlaseni() {
+        document.getElementById("prihlaseni").style.display = "block";
+        document.getElementById("registrace").style.display = "none";
+        document.getElementById("btnPri").style.display = "none";
+        document.getElementById("btnReg").style.display = "block";
+    }
+
+    function closePrihlaseni() {
+        document.getElementById("prihlaseni").style.display = "none";
+        document.getElementById("registrace").style.display = "none";
+        document.getElementById("btnPri").style.display = "block";
+        document.getElementById("btnReg").style.display = "none";
+    }
+
+    function openRegistrace() {
+        document.getElementById("prihlaseni").style.display = "none";
+        document.getElementById("registrace").style.display = "block";
+        document.getElementById("btnPri").style.display = "none";
+        document.getElementById("btnReg").style.display = "none";
+    }
+
+    function closeRegistrace() {
+        document.getElementById("prihlaseni").style.display = "block";
+        document.getElementById("registrace").style.display = "none";
+        document.getElementById("btnPri").style.display = "none";
+        document.getElementById("btnReg").style.display = "block";
+    }
+</script>
---
+++ profil.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<?php
+include('db.php');
+include('head.php');
+include('Includes/funkce.php');
+?>
+
+<body>
+    <?php include('header.php'); ?>
+    <a href="index.php">
+        <h1 class="nadpis">CINETEK</h1>
+    </a>
+    <div class="maindiv">
+        <div class="blok">
+        <h2>Přihlásit se</h2>
+        </div>
+        
+    </div>
+    <?php include('footer.php'); ?>
+</body>
+
+</html>
