75f56179095195daf83c855190240ab3ed90ae34
ZettaiNi <vojta.ondracka@gmail.com>
2023-04-01 16:03:35 +0200

    progres

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer>
     <p>Maturitní práce 2022/23<br>Aplikovaná informatika</p>
     <p>Vojtěch Ondračka</p>
-    <p>Poslední update: 30.1.2023</p>
+    <p>Poslední update: 30.3.2023</p>
 </footer>
